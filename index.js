286249e2b7e2f0e423c3a520184fb2cdde7bbf57
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-11-16 12:59:29 +0200

    Updated the javascript file

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ document.getElementById("menu").addEventListener("click", () => {
     x.className = "right";
   }
 });
+
+// function for background page
+function changeBackgroundImage() {
+  //element i'm changing
+  let sectionElement = document.querySelector(".one");
+
+  //array with images
+  const images = [
+    "url(images/tesla-section-1-bg.avif)",
+    "url(images/tesla-section-1-slide-2.avif)",
+  ];
+
+  //index for the images
+  let currentIndex = 0;
+
+  //interval function
+  setInterval(() => {
+    sectionElement.style.backgroundImage = images[currentIndex];
+
+    //toggling between images
+    currentIndex = (currentIndex + 1) % images.length;
+  }, 60000);
+}
+
+//calling the function
+changeBackgroundImage();
